766165c83ad9c92e7a62e8ad433475cdd8037073
Panos Theodoropoulos <ptheodor@di.uoa.gr>
2018-02-03 02:34:34 +0200

    Add subdirs for static under build dir

--- setupdev.sh
+++ setupdev.sh
@@ -20,3 +20,20 @@ cd Profiling
 
 cmake -DCMAKE_BUILD_TYPE=Profiling ../..
 
+cd ..
+mkdir Release-static
+cd Release-static
+
+cmake -DCMAKE_BUILD_TYPE=Release -DSTATIC=1 ../..
+
+cd ..
+mkdir Debug-static
+cd Debug-static
+
+cmake -DCMAKE_BUILD_TYPE=Debug -DSTATIC=1 ../..
+
+cd ..
+mkdir Profiling-static
+cd Profiling-static
+
+cmake -DCMAKE_BUILD_TYPE=Profiling -DSTATIC=1 ../..
